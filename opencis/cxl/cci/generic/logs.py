opencis/opencis-core

commit 6fa4bc2f65b878a917e330c17cec3eb26a47bd46

pylint: Add __init__.py to all directories and address all issues (#175)
Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

opencis/cxl/cci/generic/logs.py
@@ -5,6 +5,8 @@
  See LICENSE for details.
 """
 
+from typing import List
+
 from opencis.cxl.features.mailbox import (
     CxlMailboxContext,
     CxlMailboxCommandBase,
@@ -22,7 +24,6 @@ from opencis.cxl.features.log_manager import (
     SupportedLogEntry,
     LogManager,
 )
-from typing import List
 
 #
 #   GetEventRecordsInput command (Opcode 0400h)
@@ -59,6 +60,7 @@ class GetSupportedLogsOutput(UnalignedBitStructure):
 
     @staticmethod
     def get_size(entries: List[SupportedLogEntry]):
+        # pylint: disable=arguments-renamed
         return 0x08 + len(entries) * SupportedLogEntry.get_size()
 
 
@@ -76,8 +78,8 @@ class GetSupportedLogs(CxlMailboxCommandBase):
 
         supported_logs = self.log_manager.get_supported_logs()
         output_length = GetSupportedLogsOutput.get_size(supported_logs)
-        output_buffer = context.payloads.create_shared(output_length)
-        output = GetSupportedLogsOutput(output_buffer, supported_logs)
+        # output_buffer = context.payloads.create_shared(output_length)
+        # output = GetSupportedLogsOutput(output_buffer, supported_logs)
         context.command["payload_length"] = output_length
         return True
 

demos/image-classification/accel-t1.py
@@ -33,7 +33,7 @@ async def shutdown(signame=None):
     except Exception as exc:
         logger.debug(f"[ACCEL] {exc.__traceback__}")
     finally:
-        sys.exit(0)
+        os._exit(0)
 
 
 async def main():

opencis/apps/multi_logical_device.py
@@ -20,7 +20,6 @@ class MultiLogicalDevice(RunnableComponent):
     def __init__(
         self,
         port_index: int,
-        ld_count: int,
         memory_sizes: List[int],
         memory_files: List[str],
         serial_numbers: List[str],
@@ -38,10 +37,7 @@ class MultiLogicalDevice(RunnableComponent):
         assert len(memory_sizes) == len(
             memory_files
         ), "memory_sizes, and memory_files must have the same length"
-        assert ld_count == len(
-            serial_numbers
-        ), "ld_count must be equal to the number of serial_numbers"
-        assert ld_count == len(memory_sizes), "ld_count must be equal to the number of memory_sizes"
+        ld_count = len(memory_sizes)
 
         assert (
             not test_mode or cxl_connections is not None

opencis/bin/accelerator.py
@@ -6,9 +6,9 @@
 """
 
 import asyncio
-import click
 from enum import Enum
 from typing import List
+import click
 
 from opencis.util.logger import logger
 from opencis.cxl.environment import parse_cxl_environment
@@ -23,7 +23,6 @@ class ACCEL_TYPE(Enum):
 @click.group(name="accel")
 def accel_group():
     """Command group for managing logical devices."""
-    pass
 
 
 async def run_devices(accels: List[MyType1Accelerator | MyType2Accelerator]):
@@ -55,6 +54,6 @@ def start_group(config_file, dev_type):
                 port=cxl_env.switch_config.port,
             )
         else:
-            Exception("Invalid Aceelerator Type")
+            raise Exception("Invalid Aceelerator Type")
         accels.append(accel)
     asyncio.run(run_devices(accels))

opencis/bin/cli.py
@@ -5,13 +5,15 @@
  See LICENSE for details.
 """
 
-import click
 import os
 import sys
 import multiprocessing
 import logging
-
 from importlib import import_module
+import click
+from pylibpcap.pcap import Sniff, wpcap
+from pylibpcap.exception import LibpcapError
+
 from opencis.util.logger import logger
 from opencis.bin import fabric_manager
 from opencis.bin import get_info
@@ -28,6 +30,7 @@ def cli():
 
 
 def validate_component(ctx, param, components):
+    # pylint: disable=unused-argument
     valid_components = [
         "fm",
         "switch",
@@ -49,6 +52,7 @@ def validate_component(ctx, param, components):
 
 
 def validate_log_level(ctx, param, level):
+    # pylint: disable=unused-argument
     valid_levels = list(logging.getLevelNamesMapping().keys())
     if level:
         level = level.upper()
@@ -71,7 +75,6 @@ def validate_log_level(ctx, param, level):
 @click.option("--log-file", help="<Log File> output path.")
 @click.option("--pcap-file", help="<Packet Capture File> output path.")
 @click.option("--log-level", callback=validate_log_level, help="Specify log level.")
-@click.option("--no-hm", is_flag=True, default=False, help="Do not start HostManager.")
 @click.option("--show-timestamp", is_flag=True, default=False, help="Show timestamp.")
 @click.option("--show-loglevel", is_flag=True, default=False, help="Show log level.")
 @click.option("--show-linenumber", is_flag=True, default=False, help="Show line number.")
@@ -82,7 +85,6 @@ def start(
     log_level,
     log_file,
     pcap_file,
-    no_hm,
     show_timestamp,
     show_loglevel,
     show_linenumber,
@@ -163,19 +165,18 @@ def start(
         p_mgroup.start()
 
     if "host" in comp or "host-group" in comp:
-        hm_mode = False  # TODO: re-enable HostManager hm_mode = not no_hm
-        if hm_mode:
-            p_hm = multiprocessing.Process(target=start_host_manager, args=(ctx,))
-            processes.append(p_hm)
-            p_hm.start()
+        # TODO: Re-enable HostManager
+        # hm_mode = not no_hm
+        # if hm_mode:
+        #     p_hm = multiprocessing.Process(target=start_host_manager, args=(ctx,))
+        #     processes.append(p_hm)
+        #     p_hm.start()
         if "host" in comp:
             p_host = multiprocessing.Process(target=start_host, args=(ctx,))
             processes.append(p_host)
             p_host.start()
         elif "host-group" in comp:
-            p_hgroup = multiprocessing.Process(
-                target=start_host_group, args=(ctx, config_file, hm_mode)
-            )
+            p_hgroup = multiprocessing.Process(target=start_host_group, args=(ctx, config_file))
             processes.append(p_hgroup)
             p_hgroup.start()
 
@@ -183,20 +184,19 @@ def start(
 # helper functions
 def start_capture(ctx, pcap_file):
     def capture(pcap_file):
-        from pylibpcap.pcap import Sniff, wpcap
-        from pylibpcap.exception import LibpcapError
 
         logger.info(f"Capturing in pid: {os.getpid()}")
         if os.path.exists(pcap_file):
             os.remove(pcap_file)
 
         filter_str = (
-            "((tcp port 8000) or (tcp port 8100) or (tcp port 8200) or (tcp port 8300) or (tcp port 8400))"
-            + " and (((ip[2:2] - ((ip[0] & 0xf) << 2)) - ((tcp[12] & 0xf0) >> 2)) != 0)"
+            "((tcp port 8000) or (tcp port 8100) or (tcp port 8200) "
+            "or (tcp port 8300) or (tcp port 8400)) "
+            "and (((ip[2:2] - ((ip[0] & 0xf) << 2)) - ((tcp[12] & 0xf0) >> 2)) != 0)"
         )
         try:
             sniffobj = Sniff(iface="lo", count=-1, promisc=1, filters=filter_str)
-            for plen, t, buf in sniffobj.capture():
+            for _, _, buf in sniffobj.capture():
                 wpcap(buf, pcap_file)
                 logger.hexdump("TRACE", buf)
         except KeyboardInterrupt:
@@ -215,8 +215,8 @@ def start_capture(ctx, pcap_file):
     ctx.invoke(capture, pcap_file=pcap_file)
 
 
-def start_host_manager(ctx):
-    ctx.invoke(cxl_simple_host.start_host_manager)
+def start_host_manager():
+    pass
 
 
 def start_fabric_manager(ctx):
@@ -231,8 +231,8 @@ def start_host(ctx):
     ctx.invoke(cxl_host.start)
 
 
-def start_host_group(ctx, config_file, hm_mode):
-    ctx.invoke(cxl_host.start_group, config_file=config_file, hm_mode=hm_mode)
+def start_host_group(ctx, config_file):
+    ctx.invoke(cxl_host.start_group, config_file=config_file)
 
 
 def start_sld(ctx, config_file):
@@ -254,12 +254,6 @@ def start_mld_group(ctx, config_file):
 def start_accel_group(ctx, config_file, dev_type):
     accel = import_module("opencis.bin.accelerator")
     ctx.invoke(accel.start_group, config_file=config_file, dev_type=dev_type)
-
-
-@cli.command(name="stop")
-def foo():
-    """Stop components"""
-    pass
 
 
 cli.add_command(cxl_host.host_group)

opencis/bin/common.py
@@ -17,10 +17,10 @@ class BasedInt(click.ParamType):
         try:
             if value[:2].lower() == "0x":
                 return int(value[2:], 16)
-            else:
-                return int(value, 10)
+            return int(value, 10)
         except ValueError:
             logger.error(f"{value!r} is not a valid integer")
+        return None
 
 
 BASED_INT = BasedInt()

opencis/bin/cxl_host.py
@@ -7,33 +7,19 @@
 
 import asyncio
 import click
+
 from opencis.util.logger import logger
 from opencis.cxl.environment import parse_cxl_environment
 from opencis.cxl.component.cxl_component import PORT_TYPE
 from opencis.apps.memory_pooling import run_host
-from opencis.bin.common import BASED_INT
 
 
 @click.group(name="host")
 def host_group():
     """Command group for managing CXL Host"""
-    pass
 
 
-@host_group.command(name="reinit")
-@click.argument("port", type=BASED_INT)
-@click.option("--hpa-base", help="HPA Base Address", type=BASED_INT)
-def reinit(port, hpa_base: int):
-    client = CxlHostUtilClient()
-    try:
-        asyncio.run(client.reinit(port, hpa_base))
-    except Exception as e:
-        logger.info(f"CXL-Host[Port{port}]: {e}")
-        return
-    logger.info(f"CXL-Host[Port{port}]: Reinit done")
-
-
-def start(port: int = 0, hm_mode: bool = False):
+def start(port: int = 0):
     logger.info(f"Starting CXL Host on Port{port}")
     asyncio.run(run_host(port_index=port, irq_port=8500))
 
@@ -54,7 +40,7 @@ async def run_host_group(ports):
     await asyncio.gather(*tasks)
 
 
-def start_group(config_file: str, hm_mode: bool = True):
+def start_group(config_file: str):
     logger.info(f"Starting CXL Host Group - Config: {config_file}")
     try:
         environment = parse_cxl_environment(config_file)
@@ -67,10 +53,3 @@ def start_group(config_file: str, hm_mode: bool = True):
         if port_config.type == PORT_TYPE.USP:
             ports.append(idx)
     asyncio.run(run_host_group(ports))
-
-
-def start_host_manager():
-    logger.info(f"Starting CXL HostManager")
-    host_manager = CxlHostManager()
-    asyncio.run(host_manager.run())
-    asyncio.run(host_manager.wait_for_ready())

commit 4a9c20e41a2e87ec5f4a1969ad3cda339b823610

Fix for regression introduced by 6fa4bc2
Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

opencis/cxl/cci/generic/logs.py
@@ -78,8 +78,8 @@ class GetSupportedLogs(CxlMailboxCommandBase):
 
         supported_logs = self.log_manager.get_supported_logs()
         output_length = GetSupportedLogsOutput.get_size(supported_logs)
-        # output_buffer = context.payloads.create_shared(output_length)
-        # output = GetSupportedLogsOutput(output_buffer, supported_logs)
+        output_buffer = context.payloads.create_shared(output_length)
+        GetSupportedLogsOutput(output_buffer, supported_logs)
         context.command["payload_length"] = output_length
         return True
 