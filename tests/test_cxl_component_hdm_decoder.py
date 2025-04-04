86ad021a4b3437d04c3ec953db84ccdbd606b555
Juhyung Park <qkrwngud825@gmail.com>
2025-04-03 23:41:44 -0700

    Reformat copyright comment using Black 25.1.0
    Black's 2025 stable style requires no whitespace within multi-line comments:
    https://github.com/psf/black/issues/4522
    
    Also, update the copyright to 2024-2025:
    ```
    sed -i -e 's/Copyright (c) 2024/Copyright (c) 2024-2025/g' $(git grep -wl 2024)
    ```
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- tests/test_cxl_component_hdm_decoder.py
+++ tests/test_cxl_component_hdm_decoder.py
@@ -1,8 +1,8 @@
 """
- Copyright (c) 2024, Eeum, Inc.
+Copyright (c) 2024-2025, Eeum, Inc.
 
- This software is licensed under the terms of the Revised BSD License.
- See LICENSE for details.
+This software is licensed under the terms of the Revised BSD License.
+See LICENSE for details.
 """
 
 from opencis.cxl.component.hdm_decoder import (
