opencis/opencis-core

commit 696e444d843adfd8f3cc88f837db263769e99df6

Add Completer ID support
Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

opencis/cxl/component/virtual_switch/routers.py
@@ -205,9 +205,11 @@ class MmioRouter(CxlRouter):
         Note that data_len should be in bytes.
         """
         if data is not None:
-            packet = CxlIoCompletionWithDataPacket.create(req_id, tag, data, pload_len=data_len)
+            packet = CxlIoCompletionWithDataPacket.create(
+                req_id=req_id, tag=tag, data=data, pload_len=data_len
+            )
         else:
-            packet = CxlIoCompletionPacket.create(req_id, tag)
+            packet = CxlIoCompletionPacket.create(req_id=req_id, tag=tag)
         packet.cpl_header.req_id = 0
         await self._upstream_connection_fifo.target_to_host.put(packet)
 
@@ -309,8 +311,7 @@ class ConfigSpaceRouter(CxlRouter):
             await self._upstream_connection_fifo.target_to_host.put(packet)
 
     async def _send_unsupported_request(self, req_id, tag):
-        packet = CxlIoCompletionPacket.create(req_id, tag, CXL_IO_CPL_STATUS.UR)
-        packet.cpl_header.req_id = 0
+        packet = CxlIoCompletionPacket.create(req_id=req_id, tag=tag, status=CXL_IO_CPL_STATUS.UR)
         await self._upstream_connection_fifo.target_to_host.put(packet)
 
     async def update_router(self, vppb_index: int):

opencis/pci/component/config_space_manager.py
@@ -62,8 +62,9 @@ class ConfigSpaceManager(RunnableComponent):
         await self._downstream_fifo.host_to_target.put(packet)
 
     async def _send_unsupported_request(self, req_id, tag, ld_id):
-        packet = CxlIoCompletionPacket.create(req_id, tag, status=CXL_IO_CPL_STATUS.UR, ld_id=ld_id)
-        packet.cpl_header.req_id = self._req_id
+        packet = CxlIoCompletionPacket.create(
+            req_id=req_id, tag=tag, status=CXL_IO_CPL_STATUS.UR, ld_id=ld_id
+        )
         await self._upstream_fifo.target_to_host.put(packet)
 
     def _is_bridge(self) -> bool:
@@ -112,12 +113,15 @@ class ConfigSpaceManager(RunnableComponent):
         )
         value = self._register.read_bytes(cfg_addr, cfg_addr + size - 1)
 
-        completion_packet = CxlIoCompletionWithDataPacket.create(req_id, tag, value, ld_id=ld_id)
-        completion_packet.cpl_header.req_id = self._req_id
-        await self._upstream_fifo.target_to_host.put(completion_packet)
+        cpl_packet = CxlIoCompletionWithDataPacket.create(
+            req_id, tag, value, cpl_id=dest_id, ld_id=ld_id
+        )
+        cpl_packet.cpl_header.req_id = self._req_id
+        await self._upstream_fifo.target_to_host.put(cpl_packet)
 
     async def _process_cxl_io_cfg_wr(self, cfg_wr_packet: CxlIoCfgWrPacket):
         # NOTE: All PCIe devices are single function devices.
+        dest_id = tlptoh16(cfg_wr_packet.cfg_req_header.dest_id)
         req_id = tlptoh16(cfg_wr_packet.cfg_req_header.req_id)
         tag = cfg_wr_packet.cfg_req_header.tag
         ld_id = cfg_wr_packet.tlp_prefix.ld_id
@@ -148,9 +152,10 @@ class ConfigSpaceManager(RunnableComponent):
         )
         self._register.write_bytes(cfg_addr, cfg_addr + size - 1, value)
 
-        completion_packet = CxlIoCompletionPacket.create(req_id, tag, ld_id=ld_id)
-        completion_packet.cpl_header.req_id = self._req_id
-        await self._upstream_fifo.target_to_host.put(completion_packet)
+        cpl_packet = CxlIoCompletionPacket.create(
+            req_id=req_id, tag=tag, cpl_id=dest_id, ld_id=ld_id
+        )
+        await self._upstream_fifo.target_to_host.put(cpl_packet)
 
     async def _process_host_to_target(self):
         # pylint: disable=duplicate-code
@@ -161,7 +166,7 @@ class ConfigSpaceManager(RunnableComponent):
                 logger.debug(self._create_message("Stop processing host to target fifo"))
                 break
             base_packet = cast(CxlIoBasePacket, packet)
-            self._req_id = base_packet.cfg_req_header.req_id
+            # self._req_id = base_packet.cfg_req_header.req_id
             logger.debug(self._create_message("Received host to target packet"))
             if base_packet.is_cfg_type0():
                 if base_packet.is_cfg_read():

opencis/cxl/transport/transaction.py
@@ -22,7 +22,6 @@ from opencis.util.pci import (
     extract_bus_from_bdf,
 )
 from opencis.util.number import (
-    get_randbits,
     htotlp16,
     tlptoh16,
     extract_upper,
@@ -668,7 +667,11 @@ class CxlIoCompletionPacket(CxlIoBasePacket):
 
     @staticmethod
     def create(
-        req_id: int, tag: int, status: CXL_IO_CPL_STATUS = CXL_IO_CPL_STATUS.SC, ld_id: int = 0
+        req_id: int,
+        tag: int,
+        cpl_id: int = 0,
+        status: CXL_IO_CPL_STATUS = CXL_IO_CPL_STATUS.SC,
+        ld_id: int = 0,
     ) -> "CxlIoCompletionPacket":
         packet = CxlIoCompletionPacket()
         packet.system_header.payload_type = PAYLOAD_TYPE.CXL_IO
@@ -677,8 +680,8 @@ class CxlIoCompletionPacket(CxlIoBasePacket):
         packet.cxl_io_header.length_upper = 0b000
         packet.cxl_io_header.length_lower = 0b00000000
         packet.tlp_prefix.ld_id = ld_id
-        # TODO: actual ID to be added
-        packet.cpl_header.cpl_id = htotlp16(get_randbits(16))
+
+        packet.cpl_header.cpl_id = htotlp16(cpl_id)
         packet.cpl_header.status = status
         packet.cpl_header.byte_count_upper = 0
         packet.cpl_header.byte_count_lower = 4
@@ -711,6 +714,7 @@ class CxlIoCompletionWithDataPacket(CxlIoBasePacket):
         req_id: int,
         tag: int,
         data: int,
+        cpl_id: int = 0,
         status: CXL_IO_CPL_STATUS = CXL_IO_CPL_STATUS.SC,
         pload_len=0x04,
         ld_id: int = 0,
@@ -725,8 +729,7 @@ class CxlIoCompletionWithDataPacket(CxlIoBasePacket):
         packet.cxl_io_header.length_upper = extract_upper(pload_len // 4, 2, 10)
         packet.cxl_io_header.length_lower = extract_lower(pload_len // 4, 8, 10)
 
-        # TODO: actual ID to be added
-        packet.cpl_header.cpl_id = htotlp16(get_randbits(16))
+        packet.cpl_header.cpl_id = htotlp16(cpl_id)
         packet.cpl_header.status = status
         packet.cpl_header.req_id = htotlp16(req_id)
         packet.cpl_header.tag = tag

opencis/pci/component/mmio_manager.py
@@ -144,7 +144,7 @@ class MmioManager(PacketProcessor):
                 req_id, tag, data, pload_len=data_len, ld_id=ld_id
             )
         else:
-            packet = CxlIoCompletionPacket.create(req_id, tag, ld_id=ld_id)
+            packet = CxlIoCompletionPacket.create(req_id=req_id, tag=tag, ld_id=ld_id)
 
         packet.cpl_header.req_id = self._req_id
         await self._upstream_fifo.target_to_host.put(packet)

tests/test_cxl_component_switch_connection_manager.py
@@ -462,10 +462,13 @@ async def test_switch_connection_manager_handle_cfg_completion():
         req1 = CxlIoCfgWrPacket.create(0, 0x10, 4, 0xDEADBEEF, req_id=req_id, tag=tag)
         await client_connection.cfg_fifo.host_to_target.put(req1)
 
+        cpl_id = 0x20
         tag = 0xA6
         req2 = CxlIoCfgRdPacket.create(0, 0x10, 4, req_id=req_id, tag=tag)
         await client_connection.cfg_fifo.host_to_target.put(req2)
-        cpl2 = CxlIoCompletionWithDataPacket.create(req_id, tag, CXL_IO_CPL_STATUS.SC, 0xDEADBEEF)
+        cpl2 = CxlIoCompletionWithDataPacket.create(
+            req_id=req_id, tag=tag, status=CXL_IO_CPL_STATUS.SC, data=0xDEADBEEF, cpl_id=cpl_id
+        )
         await server_connection.cfg_fifo.target_to_host.put(cpl2)
 
         logger.info("[PyTest] Checking config space completion packets received from client")
@@ -527,10 +530,13 @@ async def test_switch_connection_manager_handle_mmio_completion():
         req1 = CxlIoMemWrPacket.create(0x10, 4, 0xDEADBEEF, req_id=req_id, tag=tag)
         await client_connection.mmio_fifo.host_to_target.put(req1)
 
+        cpl_id = 0x20
         tag = 0x2
         req2 = CxlIoMemRdPacket.create(0x10, 4, req_id=req_id, tag=tag)
         await client_connection.mmio_fifo.host_to_target.put(req2)
-        cpl2 = CxlIoCompletionWithDataPacket.create(req_id, tag, data=0)
+        cpl2 = CxlIoCompletionWithDataPacket.create(
+            req_id=req_id, tag=tag, data=0xA5A5, cpl_id=cpl_id
+        )
         await server_connection.mmio_fifo.target_to_host.put(cpl2)
 
         logger.info("[PyTest] Checking MMIO completion packets received from client")

commit 0004db94872c518c7fff207098d24546d6a758ed

Fix multiple VCS QEMU instances
Fixes: 696e444d843a ("Add Completer ID support")
Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

opencis/cxl/component/virtual_switch/routers.py
@@ -312,6 +312,7 @@ class ConfigSpaceRouter(CxlRouter):
 
     async def _send_unsupported_request(self, req_id, tag):
         packet = CxlIoCompletionPacket.create(req_id=req_id, tag=tag, status=CXL_IO_CPL_STATUS.UR)
+        packet.cpl_header.req_id = 0
         await self._upstream_connection_fifo.target_to_host.put(packet)
 
     async def update_router(self, vppb_index: int):

opencis/pci/component/config_space_manager.py
@@ -65,6 +65,7 @@ class ConfigSpaceManager(RunnableComponent):
         packet = CxlIoCompletionPacket.create(
             req_id=req_id, tag=tag, status=CXL_IO_CPL_STATUS.UR, ld_id=ld_id
         )
+        packet.cpl_header.req_id = self._req_id
         await self._upstream_fifo.target_to_host.put(packet)
 
     def _is_bridge(self) -> bool:
@@ -166,7 +167,7 @@ class ConfigSpaceManager(RunnableComponent):
                 logger.debug(self._create_message("Stop processing host to target fifo"))
                 break
             base_packet = cast(CxlIoBasePacket, packet)
-            # self._req_id = base_packet.cfg_req_header.req_id
+            self._req_id = base_packet.cfg_req_header.req_id
             logger.debug(self._create_message("Received host to target packet"))
             if base_packet.is_cfg_type0():
                 if base_packet.is_cfg_read():