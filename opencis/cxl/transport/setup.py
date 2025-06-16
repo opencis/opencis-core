cd26865ffea3c065a870935d13dd1e0d3f11f0e3
Kyeyoon Park <kyeyoon.park@eeum.io>
2025-06-15 20:18:20 -0500

    setup fix
    Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

--- opencis/cxl/transport/setup.py
+++ opencis/cxl/transport/setup.py
@@ -1,10 +1,15 @@
-# setup.py
-
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+import os
+
+# Simple setup.py that always builds inside this directory
+here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name="cxl_packets",
-    ext_modules=cythonize(["packet_base.pyx", "packet_structs.pyx"]),
-    zip_safe=False,
+    ext_modules=cythonize(
+        [
+            Extension("packet_base", [os.path.join(here, "packet_base.pyx")]),
+            Extension("packet_structs", [os.path.join(here, "packet_structs.pyx")]),
+        ]
+    ),
 )
